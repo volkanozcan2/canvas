0f7a471a71c3340c71e3eae625980b6f93d0f542
v8o6@ya.ru <vozcan86@googlemail.com>
2019-05-08 00:18:52 +0300

    initial

---
+++ public/javascripts/src.js
@@ -0,0 +1,40 @@
+let opts = {
+    transparent: false,
+    backgroundColor: 0x49DCB1,
+    resolution: 1,
+    antialias: 0,
+    autoResize: true,
+    forceCanvas: false,
+    clearBeforeRender: true
+};
+const app = new PIXI.Application(innerWidth, innerHeight, opts);
+document.body.appendChild(app.view);
+// create a new Sprite from an image path.
+for (let i = 0; i < 12000; i++) {
+
+    const bunny = PIXI.Sprite.from('images/bunny.png');
+    bunny.y = app.screen.height * .5;
+    bunny.x = app.screen.width * .5;
+    bunny.angle = Math.random() * Math.PI * 2;
+    bunny.scale.x *= .5;
+    bunny.scale.y *= .5;
+    bunny.length = Math.random() * 3;
+    bunny.anchor.set(0.5);
+    app.stage.addChild(bunny);
+}
+
+app.ticker.add(() => {
+    // just for fun, let's rotate mr rabbit a little
+    for (var i = 0; i < app.stage.children.length; i++) {
+        let bu = app.stage.children[i]
+        bu.x += Math.cos(bu.angle) * bu.length;
+        bu.y += Math.sin(bu.angle) * bu.length;
+
+    }
+});
+window.addEventListener('resize', resize);
+
+function resize() {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    rect.position.set(app.screen.width, app.screen.height);
+}
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var { randomBytes } = require("crypto");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'DeMo' });
+});
+router.post('/', function(req, res, next) {
+    let buf = req.body.test;
+    let buff = new Buffer(buf);
+    res.json({ status: `${buff.toString('base64')}||deneme` });
+});
+module.exports = router;
